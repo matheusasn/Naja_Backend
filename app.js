672aa755fd2adba737f44f6817e92433ebde80e9
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2019-10-01 16:00:29 -0300

    xdfgbd

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
-const express = require("express");
-const app = express();
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// mongodb+srv://usuario_admin:<password>@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority
+const app = express();
 
-const url = 'mongodb+srv://user_admin:CfWEe82UIIHkQRGf@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect('mongodb+srv://user_admin:CfWEe82UIIHkQRGf@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority', {
+    reconnectTries: Number.MAX_VALUE, 
+    useNewUrlParser: true,
+    reconnectInterval: 500,
+    poolSize: 5,
+    useUnifiedTopology: true 
+});
 
-const options = { reconnectTries: Number.MAX_VALUE, 
-                  useNewUrlParser: true,             
-                  reconnectInterval: 500, 
-                  poolSize: 5, 
-                  useUnifiedTopology: true 
-                };
-                
-mongoose.connect(url, options);
-
-mongoose.set('useCreateIndex', true);
+mongoose.connection.on('connected', () => {
+    console.log('Conectado com o banco de dados MLab!');
+})
 
 mongoose.connection.on('error', (err) => {
-    console.log('Erro na conexão com o banco de dados: ' + err);
+    console.log("Erro na conexão com o banco de dados MLab: " + err);
 });
 
-mongoose.connection.on('disconnected', () =>{
-    console.log('Aplicação desconectada do banco dados!');
-});
 
-mongoose.connection.on('connected', () =>{
-    console.log('"Servidor rodando..."');
-});
-
-//BODY PARSER
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
-//Conectado as rotas
-//require('./index')(app);
-// require('./Item')(app);
-// require('./UserRoute')(app);
-// require('./ProjectControllers')(app);
 
-//http://localhost:3000
-app.listen(process.env.PORT || 4444);
-
-module.exports = app;
+let port = process.env.PORT || 4444;
+app.listen(port);
