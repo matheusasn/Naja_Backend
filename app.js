4cd844de62fdd5e93d67c5ee557f0d9aaed13710
Matheus Augusto Silva Do Nascimento <42254757+matheusasn@users.noreply.github.com>
2019-10-01 15:50:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
-const express = require("express");
-const app = express();
+
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// mongodb+srv://usuario_admin:<password>@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority
+const ItemRoutes = require('./routes/ItemRoutes');
+const UserRoutes = require('./routes/UserRoutes');
 
-const url = 'mongodb+srv://user_admin:CfWEe82UIIHkQRGf@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority';
+const server = express();
 
-const options = { reconnectTries: Number.MAX_VALUE, 
-                  useNewUrlParser: true,             
-                  reconnectInterval: 500, 
-                  poolSize: 5, 
-                  useUnifiedTopology: true 
-                };
-                
-mongoose.connect(url, options);
+mongoose.connect('mongodb+srv://user_admin:CfWEe82UIIHkQRGf@apicluster-dxpzb.mongodb.net/test?retryWrites=true&w=majority', {
+    reconnectTries: Number.MAX_VALUE, 
+    useNewUrlParser: true,
+    reconnectInterval: 500,
+    poolSize: 5,
+    useUnifiedTopology: true 
+});
 
-mongoose.set('useCreateIndex', true);
+mongoose.connection.on('connected', () => {
+    console.log('Conectado com o banco de dados MLab!');
+})
 
 mongoose.connection.on('error', (err) => {
-    console.log('Erro na conexão com o banco de dados: ' + err);
+    console.log("Erro na conexão com o banco de dados MLab: " + err);
 });
 
-mongoose.connection.on('disconnected', () =>{
-    console.log('Aplicação desconectada do banco dados!');
-});
 
-mongoose.connection.on('connected', () =>{
-    console.log('"Servidor rodando..."');
-});
+server.use(express.json());
+server.use(ItemRoutes);
+server.use(UserRoutes);
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
-//BODY PARSER
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
-//Conectado as rotas
-//require('./index')(app);
-// require('./Item')(app);
-// require('./UserRoute')(app);
-// require('./ProjectControllers')(app);
-
-//http://localhost:3000
-app.listen(process.env.PORT || 4444);
-
-module.exports = app;
+let port = process.env.PORT || 4444;
+server.listen(port);
